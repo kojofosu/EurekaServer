34ac0fa4aa8c577ffba0e704bdfac5e39f04dd9b
kojofosu edue <kojofosube@gmail.com>
2020-12-16 17:14:36 +0000

    init

---
+++ src/test/java/com/edue/eureka/EurekaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.edue.eureka;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EurekaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
